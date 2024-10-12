278203a22937883068e42923bd67fa7579da9127
包峰華 <apaopao@baofenghuadeMacBook-Air.local>
2024-10-12 16:55:46 +0800

    一次宣告多個變數練習（補

--- practice/variablePractice.go
+++ practice/variablePractice.go
@@ -5,7 +5,15 @@ import "fmt"
 // 宣告方式 var 變數名稱 型別 ＝ 值
 var name string = "Koli"
 
+// 一次宣告多個變數
+var (
+	character string = "Wife"
+	age       string = "32"
+	marryDate string = "2021/02/16"
+)
+
 func Start() {
 	var person string = "cute"
-	fmt.Println(name, person)
+	fmt.Println("單一宣告結果:", name, person)
+	fmt.Println("一次宣告多個:", character, age, marryDate)
 }

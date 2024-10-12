f4e8e116da328251bbb346519667a89b44f3f417
包峰華 <apaopao@baofenghuadeMacBook-Air.local>
2024-10-12 16:59:42 +0800

    替換一次宣告多變數練習例子

--- practice/variablePractice.go
+++ practice/variablePractice.go
@@ -1,19 +1,22 @@
 package practice
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // 宣告方式 var 變數名稱 型別 ＝ 值
 var name string = "Koli"
 
 // 一次宣告多個變數
 var (
-	character string = "Wife"
-	age       string = "32"
-	marryDate string = "2021/02/16"
+	character  string    = "Wife"
+	isFemale   bool      = true
+	updateTime time.Time = time.Now()
 )
 
 func Start() {
 	var person string = "cute"
 	fmt.Println("單一宣告結果:", name, person)
-	fmt.Println("一次宣告多個:", character, age, marryDate)
+	fmt.Println("一次宣告多個:", character, isFemale, updateTime)
 }

32dc3046b62b1f62d13394d353993ff636ab211f
包峰華 <apaopao@baofenghuadeMacBook-Air.local>
2024-10-20 16:55:35 +0800

    省略型別與初始值宣告

--- practice/variablePractice.go
+++ practice/variablePractice.go
@@ -15,8 +15,24 @@ var (
 	updateTime time.Time = time.Now()
 )
 
+// 可省略型別和初始值
+var (
+	omitCharacter  = "Wife"
+	omitIsFemale   bool //按書上來說預設值會是zero value,但這邊是false
+	omitUpdateTime = time.Now()
+)
+
 func Start() {
+	declarPractice()
+	omitPractice()
+}
+
+func declarPractice() {
 	var person string = "cute"
 	fmt.Println("單一宣告結果:", name, person)
 	fmt.Println("一次宣告多個:", character, isFemale, updateTime)
+}
+
+func omitPractice() {
+	fmt.Println("省略練習結果:", omitCharacter, omitIsFemale, omitUpdateTime)
 }

792692690d9452ee8561dacc8eed9bb7175db955
包峰華 <apaopao@baofenghuadeMacBook-Air.local>
2024-10-20 18:40:22 +0800

    完整變數筆記

--- practice/variablePractice.go
+++ practice/variablePractice.go
@@ -23,16 +23,52 @@ var (
 )
 
 func Start() {
-	declarPractice()
+	declarePractice()
 	omitPractice()
+	shotDeclarePractice()
+	oneLineDeclarePractice()
 }
 
-func declarPractice() {
+func declarePractice() {
 	var person string = "cute"
+	fmt.Println("--------------------變數練習--------------------")
 	fmt.Println("單一宣告結果:", name, person)
 	fmt.Println("一次宣告多個:", character, isFemale, updateTime)
 }
 
 func omitPractice() {
+	fmt.Println("--------------------省略變數練習--------------------")
+	//下面這樣會出錯，因為go會分不出seed是int還是int64，但rand.Seed需要的是int64
+	// var seed = 123456789
+	// rand.Seed(seed)
 	fmt.Println("省略練習結果:", omitCharacter, omitIsFemale, omitUpdateTime)
+}
+
+func shotDeclarePractice() {
+	fmt.Println("--------------------短宣告練習--------------------")
+	shotCharacter := "wife"
+	shotIsFemale := true
+
+	fmt.Println("短宣告練習:", shotCharacter, shotIsFemale)
+
+	shotName, shotAge, shotIsGay, shotUpdateTime := "Pao", 32, false, time.Now()
+	fmt.Println("一次多個短宣告練習:", shotName, shotAge, shotIsGay, shotUpdateTime)
+
+	shotFamilyCharacter, shotFamilyAge, shotUpdateFamilyTime := getFamily()
+	fmt.Println("一次多個短宣告接回傳值練習:", shotFamilyCharacter, shotFamilyAge, shotUpdateFamilyTime)
+}
+
+func oneLineDeclarePractice() {
+	fmt.Println("--------------------var單行練習--------------------")
+	var name, character, nickName string = "Pao", "PaoPao", "PPPPao"
+	fmt.Println("var單行宣告:", name, character, nickName)
+	var name2, age, isFemale = "Koli", 32, true
+	fmt.Println("var單行宣告不同型別:", name2, age, isFemale)
+	var character2, age2, updateTime = getFamily()
+	fmt.Println("var單行宣告接回傳值:", character2, age2, updateTime)
+
+}
+
+func getFamily() (string, int, time.Time) {
+	return "Dad", 70, time.Now()
 }

a05d4b7a85d1c6642e2568b8468a2d5a21b4a3a6
包峰華 <apaopao@baofenghuadeMacBook-Air.local>
2024-10-27 09:33:37 +0800

    新增指標取值練習

--- practice/pointersPractice.go
+++ practice/pointersPractice.go
@@ -6,6 +6,11 @@ import (
 )
 
 func StartPointersPractice() {
+	variablePointer()
+	obtainValueFormPointer()
+}
+
+func variablePointer() {
 	fmt.Println("--------------指標宣告練習--------------")
 	var count1 *int
 	fmt.Printf("count1: %#v\n", count1)
@@ -16,4 +21,31 @@ func StartPointersPractice() {
 	fmt.Printf("count3: %#v\n", count3)
 	time := &time.Timer{}
 	fmt.Printf("time: %#v\n", time)
+}
+
+func obtainValueFormPointer() {
+	fmt.Println("--------------指標取值練習--------------")
+	var count1 *int
+	count2 := new(int)
+	countTemp := 6
+	count3 := &countTemp
+	nowTime := time.Now()
+	tempTime := &nowTime
+
+	if count1 != nil {
+		fmt.Printf("count1: %#v\n", *count1)
+	}
+
+	if count2 != nil {
+		fmt.Printf("count2: %#v\n", *count2)
+	}
+
+	if count3 != nil {
+		fmt.Printf("count3: %#v\n", *count3)
+	}
+
+	if tempTime != nil {
+		fmt.Printf("time: %#v\n", *tempTime)
+		fmt.Printf("time round two: %#v\n", tempTime.String())
+	}
 }
